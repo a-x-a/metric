423496288eadcfde2af736aaeff5341546b513a8
axa <a.x.a@inbox.ru>
2024-03-12 09:02:08 +1100

    add package config

--- internal/app/agent.go
+++ internal/app/agent.go
@@ -2,63 +2,22 @@ package app
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"time"
 
+	"github.com/a-x-a/go-metric/internal/config"
 	"github.com/a-x-a/go-metric/internal/models/metric"
 	"github.com/a-x-a/go-metric/internal/sender"
-
-	"github.com/caarlos0/env/v6"
 )
 
 type (
-	agentConfig struct {
-		// PollInterval - частота обновления метрик, по умолчанию 2 сек
-		PollInterval time.Duration `env:"POLL_INTERVAL"`
-		// ReportInterval - частота отправки метрик на сервер, по умолчанию 10 сек
-		ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-		// ServerAddress - адрес сервера сбора метрик
-		ServerAddress string `env:"ADDRESS"`
-	}
 	agent struct {
-		Config agentConfig
+		Config config.AgentConfig
 	}
 )
 
-func newAgentConfig() agentConfig {
-	cfg := agentConfig{
-		PollInterval:   2 * time.Second,
-		ReportInterval: 10 * time.Second,
-		ServerAddress:  "localhost:8080",
-	}
-
-	pollInterval := 2
-	reportInterval := 10
-
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Использование:\n")
-		flag.PrintDefaults()
-	}
-
-	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "адрес и порт сервера сбора метрик")
-	flag.IntVar(&pollInterval, "p", pollInterval, "частота обновления метрик")
-	flag.IntVar(&reportInterval, "r", reportInterval, "частота отправки метрик на сервер")
-	// flag.DurationVar(&cfg.PollInterval, "p", cfg.PollInterval, "частота обновления метрик")
-	// flag.DurationVar(&cfg.ReportInterval, "r", cfg.ReportInterval, "частота отправки метрик на сервер")
-
-	flag.Parse()
-
-	cfg.PollInterval = time.Duration(pollInterval) * time.Second
-	cfg.ReportInterval = time.Duration(reportInterval) * time.Second
-
-	_ = env.Parse(&cfg)
-
-	return cfg
-}
-
 func NewAgent() *agent {
-	return &agent{Config: newAgentConfig()}
+	return &agent{Config: config.NewAgentConfig()}
 }
 
 func (app *agent) Poll(ctx context.Context, metrics *metric.Metrics) {
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,15 +1,13 @@
 package app
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 
+	"github.com/a-x-a/go-metric/internal/config"
 	"github.com/a-x-a/go-metric/internal/handler"
 	"github.com/a-x-a/go-metric/internal/service/metricservice"
 	"github.com/a-x-a/go-metric/internal/storage"
-
-	"github.com/caarlos0/env/v6"
 )
 
 type (
@@ -17,39 +15,15 @@ type (
 		Run() error
 	}
 
-	serverConfig struct {
-		// ListenAddress - адрес сервера сбора метрик
-		ListenAddress string `env:"ADDRESS"`
-	}
-
 	server struct {
-		Config  serverConfig
+		Config  config.ServerConfig
 		Storage storage.Storage
 	}
 )
 
-func newServerConfig() serverConfig {
-	cfg := serverConfig{
-		ListenAddress: "localhost:8080",
-	}
-
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "Использование:\n")
-		flag.PrintDefaults()
-	}
-
-	flag.StringVar(&cfg.ListenAddress, "a", cfg.ListenAddress, "адрес и порт сервера сбора метрик")
-
-	flag.Parse()
-
-	_ = env.Parse(&cfg)
-
-	return cfg
-}
-
 func NewServer() *server {
 	return &server{
-		Config:  newServerConfig(),
+		Config:  config.NewServerConfig(),
 		Storage: storage.NewMemStorage(),
 	}
 }
@@ -60,11 +34,5 @@ func (s *server) Run() error {
 
 	fmt.Println("listening on", s.Config.ListenAddress)
 
-	err := http.ListenAndServe(s.Config.ListenAddress, r)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return http.ListenAndServe(s.Config.ListenAddress, r)
 }
