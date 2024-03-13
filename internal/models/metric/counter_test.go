a182d4295a6878eac010b6edb5dd6b45df376c76
axa <a.x.a@inbox.ru>
2024-03-13 22:18:23 +1100

    add counter/gauge test

--- internal/models/metric/counter_test.go
+++ internal/models/metric/counter_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCounter_Kind(t *testing.T) {
@@ -40,6 +41,56 @@ func TestCounter_String(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, tt.c.String())
+		})
+	}
+}
+
+func TestCounter_IsCounter(t *testing.T) {
+	tests := []struct {
+		name string
+		m    Metric
+		want bool
+	}{
+		{
+			name: "counter",
+			m:    Counter(12),
+			want: true,
+		},
+		{
+			name: "gauge",
+			m:    Gauge(12.34),
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.IsCounter()
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestCounter_IsGauge(t *testing.T) {
+	tests := []struct {
+		name string
+		m    Metric
+		want bool
+	}{
+		{
+			name: "counter",
+			m:    Counter(12),
+			want: false,
+		},
+		{
+			name: "gauge",
+			m:    Gauge(12.34),
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.IsGauge()
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
--- internal/models/metric/gauge_test.go
+++ internal/models/metric/gauge_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGauge_Kind(t *testing.T) {
@@ -45,6 +46,56 @@ func TestGauge_String(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.Equal(t, tt.want, tt.g.String())
+		})
+	}
+}
+
+func TestGauge_IsCounter(t *testing.T) {
+	tests := []struct {
+		name string
+		m    Metric
+		want bool
+	}{
+		{
+			name: "counter",
+			m:    Counter(12),
+			want: true,
+		},
+		{
+			name: "gauge",
+			m:    Gauge(12.34),
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.IsCounter()
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestGauge_IsGauge(t *testing.T) {
+	tests := []struct {
+		name string
+		m    Metric
+		want bool
+	}{
+		{
+			name: "counter",
+			m:    Counter(12),
+			want: false,
+		},
+		{
+			name: "gauge",
+			m:    Gauge(12.34),
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.IsGauge()
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
