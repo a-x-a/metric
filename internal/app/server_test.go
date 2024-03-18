df961bd68e6c7a5fa371b7681ede540f9bb76aa9
axa <a.x.a@inbox.ru>
2024-03-18 13:21:35 +1100

    upd server tst

--- internal/app/server_test.go
+++ internal/app/server_test.go
@@ -33,13 +33,13 @@ func Test_serverRun(t *testing.T) {
 		{
 			name:    "server run normal",
 			s:       &srv,
-			a:       "localhost:8081",
+			a:       "localhost:8091",
 			wantErr: false,
 		},
 		{
 			name:    "server run error",
 			s:       &srv,
-			a:       "localhost:8081",
+			a:       "localhost:8091",
 			wantErr: true,
 		},
 	}
