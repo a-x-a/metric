eee190ba7e1937dbb53bd6040d31629aa22c1351
axa <a.x.a@inbox.ru>
2024-04-04 14:24:24 +1100

    add new config for save to file

--- internal/config/serverconfig.go
+++ internal/config/serverconfig.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/caarlos0/env"
 )
@@ -13,13 +14,27 @@ type (
 		ListenAddress string `env:"ADDRESS"`
 		// LogLevel - уровень логирования, по умолчанию info
 		LogLevel string
+		// StoreInterval - интервал времени в секундах, по истечении которого
+		// текущие показания сервера сохраняются на диск
+		// (по умолчанию 300 секунд, значение `0` делает запись синхронной).
+		StoreInterval time.Duration `env:"STORE_INTERVAL"`
+		// FileStoregePath - полное имя файла, куда сохраняются текущие значения
+		// (по умолчанию `/tmp/metrics-db.json`, пустое значение отключает функцию записи на диск).
+		FileStoregePath string `env:"FILE_STORAGE_PATH"`
+		// RestoreOnStart - булево значение (`true/false`),
+		// определяющее, загружать или нет ранее сохранённые значения
+		// из указанного файла при старте сервера (по умолчанию `true`).
+		Restore bool `env:"RESTORE"`
 	}
 )
 
 func NewServerConfig() ServerConfig {
+	storeInterval := 300
 	cfg := ServerConfig{
-		ListenAddress: "localhost:8080",
-		LogLevel:      "info",
+		ListenAddress:   "localhost:8080",
+		LogLevel:        "info",
+		FileStoregePath: "/tmp/metrics-db.json",
+		Restore:         true,
 	}
 
 	flag.Usage = func() {
@@ -31,7 +46,21 @@ func NewServerConfig() ServerConfig {
 		flag.StringVar(&cfg.ListenAddress, "a", cfg.ListenAddress, "адрес и порт сервера сбора метрик")
 	}
 
+	if flag.Lookup("i") == nil {
+		flag.IntVar(&storeInterval, "i", storeInterval, "интервал сохранения текущих показаний сервера на диск")
+	}
+
+	if flag.Lookup("f") == nil {
+		flag.StringVar(&cfg.FileStoregePath, "f", cfg.FileStoregePath, "полное имя файла, куда сохраняются текущие значения")
+	}
+
+	if flag.Lookup("r") == nil {
+		flag.BoolVar(&cfg.Restore, "r", cfg.Restore, "загружать или нет ранее сохранённые значения из файла при старте")
+	}
+
 	flag.Parse()
+
+	cfg.StoreInterval = time.Duration(storeInterval) * time.Second
 
 	_ = env.Parse(&cfg)
 
