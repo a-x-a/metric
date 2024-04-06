1f400b0dda69d7d3aaca1294bd65d86c935c4e39
axa <a.x.a@inbox.ru>
2024-04-07 01:38:59 +1100

    fix test

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -8,16 +8,18 @@ import (
 )
 
 func TestNewDataStorage(t *testing.T) {
+	require := require.New(t)
+
 	fileName := os.TempDir() + string(os.PathSeparator) + "test_123456789.json"
 
 	t.Run("storage without file", func(t *testing.T) {
 		ds := NewDataStorage("", 0)
-		require.NotNil(t, ds)
+		require.NotNil(ds)
 	})
 
 	t.Run("storage with file", func(t *testing.T) {
 		ds := NewDataStorage(fileName, 0)
-		require.NotNil(t, ds)
+		require.NotNil(ds)
 	})
 
 }
