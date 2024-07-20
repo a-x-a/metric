c5b3592ab0330922e572fb37e522d47053bb126c
axa <a.x.a@inbox.ru>
2024-07-21 01:35:29 +1100

    add flag -config

--- internal/config/agentconfig.go
+++ internal/config/agentconfig.go
@@ -1,7 +1,7 @@
-// Package config инициализирует настройки агента.
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"time"
@@ -13,74 +13,140 @@ type (
 	// AgentConfig настройки агента.
 	AgentConfig struct {
 		// PollInterval частота обновления метрик, по умолчанию 2 сек
-		PollInterval time.Duration `env:"POLL_INTERVAL"`
+		PollInterval time.Duration `env:"POLL_INTERVAL" json:"poll_interval"`
 		// ReportInterval частота отправки метрик на сервер, по умолчанию 10 сек
-		ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-		// ServerAddress адрес сервера сбора метрик
-		ServerAddress string `env:"ADDRESS"`
-		// Key ключ подписи
-		Key string `env:"KEY"`
+		ReportInterval time.Duration `env:"REPORT_INTERVAL" json:"report_interval"`
 		// RateLimit количество одновременно исходящих запросов на сервер
-		RateLimit int `env:"RATE_LIMIT"`
-		//CryptoKey путь до файла с публичным ключом
-		CryptoKey string `env:"CRYPTO_KEY"`
+		RateLimit int `env:"RATE_LIMIT" json:"rate_limit"`
+		// ServerAddress адрес сервера сбора метрик
+		ServerAddress string `env:"ADDRESS" json:"address"`
+		// Key ключ подписи
+		Key string `env:"KEY" json:"key"`
+		//CryptoKey путь до файла с публичным ключом в фомате PEM
+		CryptoKey string `env:"CRYPTO_KEY" json:"crypto_key"`
 	}
 )
 
+func (cfg *AgentConfig) UnmarshalJSON(b []byte) error {
+	var err error
+	var tmp struct {
+		PollInterval   string `json:"poll_interval"`
+		ReportInterval string `json:"report_interval"`
+		RateLimit      int    `json:"rate_limit"`
+		ServerAddress  string `json:"address"`
+		Key            string `json:"key"`
+		CryptoKey      string `json:"crypto_key"`
+	}
+
+	if err = json.Unmarshal(b, &tmp); err != nil {
+		return err
+	}
+
+	cfg.PollInterval, err = time.ParseDuration(tmp.PollInterval)
+	if err != nil {
+		return err
+	}
+
+	cfg.ReportInterval, err = time.ParseDuration(tmp.ReportInterval)
+	if err != nil {
+		return err
+	}
+
+	cfg.RateLimit = tmp.RateLimit
+	cfg.ServerAddress = tmp.ServerAddress
+	cfg.Key = tmp.Key
+	cfg.CryptoKey = tmp.CryptoKey
+
+	return nil
+}
+
 // NewAgentConfig создаёт экземпляр настроек агента.
 func NewAgentConfig() AgentConfig {
-	pollInterval := 2
-	reportInterval := 10
-	serverAddress := "localhost:8080"
-	key := ""
-	rateLimit := 1
-	cryptoKey := ""
+	return AgentConfig{
+		PollInterval:   time.Duration(2) * time.Second,
+		ReportInterval: time.Duration(10) * time.Second,
+		RateLimit:      1,
+		ServerAddress:  "localhost:8080",
+		Key:            "",
+		CryptoKey:      "",
+	}
+}
 
+func (cfg *AgentConfig) Parse() error {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Использование:\n")
 		flag.PrintDefaults()
 	}
 
+	serverAddress := cfg.ServerAddress
 	if flag.Lookup("a") == nil {
 		flag.StringVar(&serverAddress, "a", serverAddress, "адрес и порт сервера сбора метрик")
 	}
 
+	pollInterval := 2
 	if flag.Lookup("p") == nil {
 		flag.IntVar(&pollInterval, "p", pollInterval, "частота обновления метрик")
 	}
 
+	reportInterval := 10
 	if flag.Lookup("r") == nil {
 		flag.IntVar(&reportInterval, "r", reportInterval, "частота отправки метрик на сервер")
 	}
 
+	key := cfg.Key
 	if flag.Lookup("k") == nil {
 		flag.StringVar(&key, "k", key, "ключ подписи")
 	}
 
+	rateLimit := int(cfg.RateLimit)
 	if flag.Lookup("l") == nil {
 		flag.IntVar(&rateLimit, "l", rateLimit, "количество одновременно исходящих запросов на сервер")
 	}
 
+	cryptoKey := cfg.CryptoKey
 	if flag.Lookup("crypto-key") == nil {
-		flag.StringVar(&cryptoKey, "crypto-key", cryptoKey, "путь до файла с публичным ключом")
+		flag.StringVar(&cryptoKey, "crypto-key", cryptoKey, "путь до файла с публичным ключом в формате PEM")
+	}
+
+	configFile := ""
+	if flag.Lookup("config") == nil && flag.Lookup("c") == nil {
+		flag.StringVar(&configFile, "config", configFile, "путь до конфигурационного файла в формате JSON")
+		flag.StringVar(&configFile, "c", configFile, "путь до конфигурационного файла в формате JSON (короткиф формат)")
 	}
 
 	flag.Parse()
 
-	cfg := AgentConfig{
-		PollInterval:   time.Duration(pollInterval) * time.Second,
-		ReportInterval: time.Duration(reportInterval) * time.Second,
-		ServerAddress:  serverAddress,
-		Key:            key,
-		RateLimit:      rateLimit,
-		CryptoKey:      cryptoKey,
+	if len(configFile) != 0 {
+		if err := loadConfigFromFile(configFile, cfg); err != nil {
+			return err
+		}
 	}
 
-	_ = env.Parse(&cfg)
+	flag.Visit(func(f *flag.Flag) {
+		switch f.Name {
+		case "a":
+			cfg.ServerAddress = serverAddress
+		case "p":
+			cfg.PollInterval = time.Duration(pollInterval) * time.Second
+		case "r":
+			cfg.ReportInterval = time.Duration(reportInterval) * time.Second
+		case "k":
+			cfg.Key = key
+		case "l":
+			cfg.RateLimit = rateLimit
+		case "crypto-key":
+			cfg.CryptoKey = cryptoKey
+		}
+	})
+
+	err := env.Parse(cfg)
+	if err != nil {
+		return err
+	}
 
 	if cfg.RateLimit < 1 {
 		cfg.RateLimit = 1
 	}
 
-	return cfg
+	return nil
 }
--- internal/config/serverconfig.go
+++ internal/config/serverconfig.go
@@ -1,7 +1,7 @@
-// Package config инициализирует настройки сервера.
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"time"
@@ -13,26 +13,57 @@ type (
 	// ServerConfig - настройки сервера.
 	ServerConfig struct {
 		// ListenAddress - адрес сервера сбора метрик
-		ListenAddress string `env:"ADDRESS"`
+		ListenAddress string `env:"ADDRESS" json:"address"`
 		// StoreInterval - интервал времени в секундах, по истечении которого
 		// текущие показания сервера сохраняются на диск
 		// (по умолчанию 300 секунд, значение `0` делает запись синхронной).
-		StoreInterval time.Duration `env:"STORE_INTERVAL"`
+		StoreInterval time.Duration `env:"STORE_INTERVAL" json:"store_interval"`
 		// FileStoregePath - полное имя файла, куда сохраняются текущие значения
 		// (по умолчанию `/tmp/metrics-db.json`, пустое значение отключает функцию записи на диск).
-		FileStoregePath string `env:"FILE_STORAGE_PATH"`
+		FileStoregePath string `env:"FILE_STORAGE_PATH" json:"store_file"`
 		// RestoreOnStart - булево значение (`true/false`),
 		// определяющее, загружать или нет ранее сохранённые значения
 		// из указанного файла при старте сервера (по умолчанию `true`).
-		Restore bool `env:"RESTORE"`
+		Restore bool `env:"RESTORE" json:"restore"`
 		// DatabaseDSN - строка с адресом подключения к БД.
-		DatabaseDSN string `env:"DATABASE_DSN"`
+		DatabaseDSN string `env:"DATABASE_DSN" json:"database_dsn"`
 		// Key - ключ подписи
-		Key string `env:"KEY"`
+		Key string `env:"KEY" json:"key"`
 		//CryptoKey - путь до файла с приватным ключом
-		CryptoKey string `env:"CRYPTO_KEY"`
+		CryptoKey string `env:"CRYPTO_KEY" json:"crypto_key"`
 	}
 )
+
+func (cfg *ServerConfig) UnmarshalJSON(b []byte) error {
+	var err error
+	var tmp struct {
+		ListenAddress   string `json:"address"`
+		StoreInterval   string `json:"store_interval"`
+		FileStoregePath string `json:"store_file"`
+		Restore         bool   `json:"restore"`
+		DatabaseDSN     string `json:"database_dsn"`
+		Key             string `json:"key"`
+		CryptoKey       string `json:"crypto_key"`
+	}
+
+	if err = json.Unmarshal(b, &tmp); err != nil {
+		return err
+	}
+
+	cfg.StoreInterval, err = time.ParseDuration(tmp.StoreInterval)
+	if err != nil {
+		return err
+	}
+
+	cfg.ListenAddress = tmp.ListenAddress
+	cfg.FileStoregePath = tmp.FileStoregePath
+	cfg.Restore = tmp.Restore
+	cfg.DatabaseDSN = tmp.DatabaseDSN
+	cfg.Key = tmp.Key
+	cfg.CryptoKey = tmp.CryptoKey
+
+	return nil
+}
 
 // NewServerConfig - создаёт экземпляр настроек сервера.
 func NewServerConfig() ServerConfig {
@@ -44,46 +75,90 @@ func NewServerConfig() ServerConfig {
 		DatabaseDSN:     "",
 		Key:             "",
 		CryptoKey:       "",
+		StoreInterval:   time.Duration(storeInterval) * time.Second,
 	}
 
+	return cfg
+}
+
+func (cfg *ServerConfig) Parse() error {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Использование:\n")
 		flag.PrintDefaults()
 	}
 
+	listenAddress := cfg.ListenAddress
 	if flag.Lookup("a") == nil {
-		flag.StringVar(&cfg.ListenAddress, "a", cfg.ListenAddress, "адрес и порт сервера сбора метрик")
+		flag.StringVar(&listenAddress, "a", listenAddress, "адрес и порт сервера сбора метрик")
 	}
 
+	storeInterval := 300
 	if flag.Lookup("i") == nil {
 		flag.IntVar(&storeInterval, "i", storeInterval, "интервал сохранения текущих показаний сервера на диск")
 	}
 
+	fileStoregePath := cfg.FileStoregePath
 	if flag.Lookup("f") == nil {
-		flag.StringVar(&cfg.FileStoregePath, "f", cfg.FileStoregePath, "полное имя файла, куда сохраняются текущие значения")
+		flag.StringVar(&fileStoregePath, "f", fileStoregePath, "полное имя файла, куда сохраняются текущие значения")
 	}
 
+	restore := cfg.Restore
 	if flag.Lookup("r") == nil {
-		flag.BoolVar(&cfg.Restore, "r", cfg.Restore, "загружать или нет ранее сохранённые значения из файла при старте")
+		flag.BoolVar(&restore, "r", restore, "загружать или нет ранее сохранённые значения из файла при старте")
 	}
 
+	databaseDSN := cfg.DatabaseDSN
 	if flag.Lookup("d") == nil {
-		flag.StringVar(&cfg.DatabaseDSN, "d", cfg.DatabaseDSN, "строка с адресом подключения к БД")
+		flag.StringVar(&databaseDSN, "d", databaseDSN, "строка с адресом подключения к БД")
 	}
 
+	key := cfg.Key
 	if flag.Lookup("k") == nil {
-		flag.StringVar(&cfg.Key, "k", cfg.Key, "ключ подписи")
+		flag.StringVar(&key, "k", key, "ключ подписи")
 	}
 
+	cryptoKey := cfg.CryptoKey
 	if flag.Lookup("crypto-key") == nil {
-		flag.StringVar(&cfg.CryptoKey, "crypto-key", cfg.CryptoKey, "путь до файла с приватным ключом")
+		flag.StringVar(&cryptoKey, "crypto-key", cryptoKey, "путь до файла с приватным ключом")
+	}
+
+	configFile := ""
+	if flag.Lookup("config") == nil && flag.Lookup("c") == nil {
+		flag.StringVar(&configFile, "config", configFile, "путь до конфигурационного файла в формате JSON")
+		flag.StringVar(&configFile, "c", configFile, "путь до конфигурационного файла в формате JSON (короткиф формат)")
 	}
 
 	flag.Parse()
 
-	cfg.StoreInterval = time.Duration(storeInterval) * time.Second
+	if len(configFile) != 0 {
+		if err := loadConfigFromFile(configFile, cfg); err != nil {
+			return err
+		}
+	}
 
-	_ = env.Parse(&cfg)
+	flag.Visit(func(f *flag.Flag) {
+		switch f.Name {
+		case "a":
+			cfg.ListenAddress = listenAddress
+		case "i":
+			cfg.StoreInterval = time.Duration(storeInterval) * time.Second
+		case "f":
+			cfg.FileStoregePath = fileStoregePath
+		case "r":
+			cfg.Restore = restore
+		case "d":
+			cfg.DatabaseDSN = databaseDSN
+		case "k":
+			cfg.Key = key
+		case "crypto-key":
+			cfg.CryptoKey = cryptoKey
+		}
+	})
 
-	return cfg
+	err := env.Parse(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
