cc55e6a7f76f0bdc4a4bd4661770e01fe689a9a0
axa <a.x.a@inbox.ru>
2024-07-24 18:49:36 +1100

    add test files for server config

--- internal/config/serverconfig_test.go
+++ internal/config/serverconfig_test.go
@@ -1,52 +1,67 @@
 package config
 
 import (
-	"encoding/json"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestServerConfig_UnmarshalJSON(t *testing.T) {
+func TestServerConfigLoadFromFile(t *testing.T) {
 	require := require.New(t)
 	tt := []struct {
-		name     string
-		src      string
-		expected ServerConfig
+		name  string
+		path  string
+		check func(t *testing.T, cfg ServerConfig, err error)
 	}{
 		{
-			name: "Load full configuration",
-			src: `{
-"address": "0.0.0.0:1234",
-"store_interval": "5s",
-"store_file": "/path/to/file.db",
-"database_dsn": "postgres://postgres:postgres@127.0.0.1:5432/praktikum?sslmode=disable",
-"restore": true,
-"key": "secret",
-"crypto_key": "./path/to/private.pem"
-}`,
-			expected: ServerConfig{
-				ListenAddress:   "0.0.0.0:1234",
-				FileStoregePath: "/path/to/file.db",
-				DatabaseDSN:     "postgres://postgres:postgres@127.0.0.1:5432/praktikum?sslmode=disable",
-				StoreInterval:   time.Duration(5) * time.Second,
-				Restore:         true,
-				Key:             "secret",
-				CryptoKey:       "./path/to/private.pem",
+			name: "load full configuration",
+			path: "testdata/full.server.config.json",
+			check: func(t *testing.T, cfg ServerConfig, err error) {
+				cfgExpected := ServerConfig{
+					ListenAddress:   "0.0.0.0:1234",
+					FileStoregePath: "/path/to/file.db",
+					DatabaseDSN:     "postgres://postgres:postgres@127.0.0.1:5432/praktikum?sslmode=disable",
+					StoreInterval:   time.Duration(5) * time.Second,
+					Restore:         true,
+					Key:             "secret",
+					CryptoKey:       "./path/to/private.pem",
+				}
+
+				require.NoError(err)
+				require.Equal(cfgExpected, cfg)
 			},
 		},
 		{
-			name: "Load partial configuration",
-			src:  `{"crypto_key": "./path/to/private.pem"}`,
-			expected: ServerConfig{
-				ListenAddress:   "localhost:8080",
-				FileStoregePath: "/tmp/metrics-db.json",
-				DatabaseDSN:     "",
-				StoreInterval:   time.Duration(300) * time.Second,
-				Restore:         true,
-				Key:             "",
-				CryptoKey:       "./path/to/private.pem",
+			name: "load partial configuration",
+			path: "testdata/partial.server.config.json",
+			check: func(t *testing.T, cfg ServerConfig, err error) {
+				cfgDefault := NewServerConfig()
+				cfgDefault.CryptoKey = "./path/to/private.pem"
+
+				require.NoError(err)
+				require.Equal(cfgDefault, cfg)
+			},
+		},
+		{
+			name: "load config with invalid json format",
+			path: "testdata/invalid.server.config.1.json",
+			check: func(t *testing.T, cfg ServerConfig, err error) {
+				require.Error(err)
+			},
+		},
+		{
+			name: "load config with invalid store_interval",
+			path: "testdata/invalid.server.config.2.json",
+			check: func(t *testing.T, cfg ServerConfig, err error) {
+				require.Error(err)
+			},
+		},
+		{
+			name: "load config file error",
+			path: "testdata/no.file.json",
+			check: func(t *testing.T, cfg ServerConfig, err error) {
+				require.Error(err)
 			},
 		},
 	}
@@ -54,37 +69,15 @@ func TestServerConfig_UnmarshalJSON(t *testing.T) {
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			cfg := NewServerConfig()
+			err := loadConfigFromFile(tc.path, &cfg)
 
-			err := json.Unmarshal([]byte(tc.src), &cfg)
-			require.NoError(err)
-
-			require.Equal(tc.expected, cfg)
+			tc.check(t, cfg, err)
 		})
 	}
 }
-
-func TestServerConfig_UnmarshallInvalidJSON(t *testing.T) {
+func TestServerConfigParse(t *testing.T) {
 	require := require.New(t)
-	tt := []struct {
-		name string
-		src  string
-	}{
-		{
-			name: "Parse config with invalid data",
-			src:  `{"address": 2}`,
-		},
-		{
-			name: "Parse config with invalid store interval",
-			src:  `{"store_interval": "_"}`,
-		},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg := NewServerConfig()
-
-			err := json.Unmarshal([]byte(tc.src), &cfg)
-			require.Error(err)
-		})
-	}
+	cfg := NewServerConfig()
+	err := cfg.Parse()
+	require.NoError(err)
 }
